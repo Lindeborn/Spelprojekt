64ff742494fd2db310710b7b8ed9a7d8b53da7b5
Johanna <johanna.dahlborn@gmail.com>
2021-05-05 15:45:37 +0200

    Menu
    Menu

--- Assets/Scenes/MainMenu.cs
+++ Assets/Scenes/MainMenu.cs
@@ -1,16 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityENgine.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
 
-public void PlayGame(){
-SceneManager.LoadScene(level)
-}
+	public void GoToSettingsScene()
+    {
+	SceneManager.LoadScene("Settingscene");
+	}
 
-public void QuitGame(){
-Application.Quit();
-}
+
+	public void GoToMainMenu()
+    {
+	SceneManager.LoadScene("MainMenu");
+	}
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
